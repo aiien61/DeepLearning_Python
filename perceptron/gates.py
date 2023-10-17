65bf93d7b288f5a7b341b633276512c19ff229c4
aiien61 <aiien61will@gmail.com>
2023-10-17 13:54:07 +0800

    initial commit

---
+++ perceptron/gates.py
@@ -0,0 +1,17 @@
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    if (x1 * w1 + x2 * w2) >= theta:
+        return 0
+    else:
+        return 1
+
+
+def main():
+    print(AND(0, 0))
+    print(AND(1, 0))
+    print(AND(0, 1))
+    print(AND(1, 1))
+
+
+if __name__ == "__main__":
+    main()
