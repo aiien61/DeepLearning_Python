cdfb0408813cd7c8ddd7ef5b1021291b0e787027
aiien61 <aiien61will@gmail.com>
2023-10-17 18:01:03 +0800

    add new activation function: ReLU

--- neural_network/activation_functions.py
+++ neural_network/activation_functions.py
@@ -42,10 +42,24 @@ def plot_step_function_and_sigmoid():
     y_sigmoid = sigmoid(x)
     y_step = step_function_numpy(x)
     plt.plot(x, y_sigmoid, label='sigmoid')
-    plt.plot(x, y_step, label='step function', linestyle='--')
+    plt.plot(x, y_step, label='step function', linestyle='dashed')
     plt.ylim(-0.1, 1.1)
     plt.legend()
     plt.show()
 
+
+# ReLU: Rectified Linear Unit
+def relu(x):
+    return np.maximum(0, x)
+
+
+def plot_relu():
+    x = np.arange(-5, 5, 0.1)
+    y = relu(x)
+    plt.plot(x, y)
+    plt.ylim(-1, 5.1)
+    plt.show()
+
+
 if __name__ == '__main__':
-    plot_step_function_and_sigmoid()
+    plot_relu()
