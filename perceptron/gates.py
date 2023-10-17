387369fbffbd3fcc75b496e1ee96ec3fa5d93c10
aiien61 <aiien61will@gmail.com>
2023-10-17 14:14:20 +0800

    bug fixes: AND gate activation equation, and refactor to vector version using numpy

--- perceptron/gates.py
+++ perceptron/gates.py
@@ -1,6 +1,11 @@
+import numpy as np
+
+
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    if (x1 * w1 + x2 * w2) >= theta:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7  # bias
+    if np.dot(x, w) + b <= 0:
         return 0
     else:
         return 1
