d011ef6a673116ef0f6fa5b6abaecee9cfbde231
aiien61 <aiien61will@gmail.com>
2023-10-25 02:11:35 +0800

    activation function layer

--- backpropagation/layers.py
+++ backpropagation/layers.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Relu:
     def __init__(self):
         self.mask = None
@@ -11,4 +14,18 @@ class Relu:
     def backward(self, dout):
         dout[self.mask] = 0
         dx = dout
+        return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
         return dx
