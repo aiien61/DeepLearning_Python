9a35e496852525dd118f448b9949a6348f25717c
aiien61 <aiien61will@gmail.com>
2023-10-25 16:43:43 +0800

    refactor two_layer_net with improved gradient method and generalised learning steps

--- neuralnet_learning/two_layer_net.py
+++ neuralnet_learning/two_layer_net.py
@@ -1,5 +1,10 @@
+import sys, os
+sys.path.append(os.pardir)
+
 import numpy as np
 from numerical_differentiation import numerical_gradient
+from backpropagation.layers import *
+from collections import OrderedDict
 
 
 sigmoid = lambda x: 1 / (1 + np.exp(-x))
@@ -20,32 +25,49 @@ def cross_entropy_error(y, t):
 
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # initialise weights
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
         self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
-    
-    def predict(self, x):
-        W1, W2 = self.params['W1'], self.params['W2']
-        b1, b2 = self.params['b1'], self.params['b2']
+        # generate layers
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.lastlayer = SoftmaxWithLoss()
 
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        y = softmax(a2)
-        return y
+
+    def predict(self, x):
+        # Manually
+        # W1, W2 = self.params['W1'], self.params['W2']
+        # b1, b2 = self.params['b1'], self.params['b2']
+
+        # a1 = np.dot(x, W1) + b1
+        # z1 = sigmoid(a1)
+        # a2 = np.dot(z1, W2) + b2
+        # y = softmax(a2)
+        # return y
+
+        # Automatically
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
     
     # x: input data, t: training data
     def loss(self, x, t):
         y = self.predict(x)
-        return cross_entropy_error(y, t)
+        
+        # return cross_entropy_error(y, t)
+        return self.lastlayer.forward(y, t)
     
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
@@ -61,6 +83,28 @@ class TwoLayerNet:
         grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
         return grads
+    
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
+
+        # backward
+        dout = 1
+        dout = self.lastlayer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # config
+        grads = {}
+        grads['W1'] = self.layers['Affine1'].dW
+        grads['b1'] = self.layers['Affine1'].db
+        grads['W2'] = self.layers['Affine2'].dW
+        grads['b2'] = self.layers['Affine2'].db
+
+        return grads
 
 
 if __name__ == "__main__":
@@ -69,7 +113,7 @@ if __name__ == "__main__":
     x = np.random.rand(100, 784)  # 100 random input data 
     t = np.random.rand(100, 10)  # 100 correct labels
 
-    grads = net.numerical_gradient(x, t)  # compute gradients
+    grads = net.gradient(x, t)  # compute gradients
 
     print(net.params['W1'].shape)
     print(net.params['b1'].shape)
