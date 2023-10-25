a33b66d3f30c3f6ed160f1240e5167a962602822
aiien61 <aiien61will@gmail.com>
2023-10-25 16:42:25 +0800

    Affine layer class and SoftmaxWithLoss layer class

--- backpropagation/layers.py
+++ backpropagation/layers.py
@@ -1,4 +1,8 @@
+import sys, os
+sys.path.append(os.pardir)
+
 import numpy as np
+from common.functions import softmax, cross_entropy_error
 
 
 class Relu:
@@ -28,4 +32,42 @@ class Sigmoid:
     
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
+        return dx
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+    
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None  # loss
+        self.y = None  # the output of softmax
+        self.t = None  # the one-hot encoding data from training data
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
         return dx
